3f24440e1f94067efa5ba8607a1ccd8fe818706b
koko2loko <courtney3koko@yahoo.com>
2022-11-17 17:43:10 -0600

    Update reservation Page

--- my-app/src/components/Reservation/Reservation.js
+++ my-app/src/components/Reservation/Reservation.js
@@ -1,21 +1,30 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import styles from './Reservation.module.css';
 
-function Reservation() { 
+function Reservation() {
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
     const firstName = useSelector(state => state.auth.firstName);
     const lastName = useSelector(state => state.auth.lastName);
 
     return (<>
-        <div>
+        <div className={styles.page}>
             <h1>Book a Reservation</h1>
-            {isLoggedIn && <>
-                <div>First Name: {firstName}</div>
-                <div>Last Name: {lastName}</div>
-            </>}
-            {!isLoggedIn && <>
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
-            </>}
+            <div className={styles.inputFields}>
+
+                {isLoggedIn && <>
+                    <div>First Name: {firstName}</div>
+                    <div>Last Name: {lastName}</div>
+                </>}
+                {!isLoggedIn && <>
+                    <input type="text" placeholder="First Name" />
+                    <input type="text" placeholder="Last Name" />
+                </>}
+                <>
+
+                    <input type="date" placeholder="Date" />
+                    <input type="number" placeholder="Number of People" />
+                </>
+            </div>
         </div>
     </>)
 }
